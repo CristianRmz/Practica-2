e10be9707a5989868e3e7a00dca1658bada4ea01
Crist <Crist@DESKTOP-RP46J8U>
2022-12-07 21:33:48 -0800

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad = 21;
+            string Nombre = "Cristian";
+            string Pais = "Rumania";
+
+            Console.WriteLine("Hola mi nombre es: {0}, tengo {1} años de edad y me gustaria conocer  {2}", Edad, Nombre, Pais);
+            Console.ReadKey();
+        }
+    }
+}
